0e26a70a3632bbc70b2d27cbc2ce60459b602159
Avelia Diva Zahra <avelia.diva@ui.ac.id>
2022-05-27 18:29:35 +0700

    matched html

--- registrasiPengguna/urls.py
+++ registrasiPengguna/urls.py
@@ -4,10 +4,6 @@ from .views import *
 
 app_name = 'registrasiPengguna'
 urlpatterns = [
-    #path('', user_reg_page, name='user_reg'),
-
-
-    path('Registrasi', pilihakun),
     path('RegistrasiAdmin', registadmin),
     path('RegistrasiPemain', registerPemain),
 ]
--- registrasiPengguna/views.py
+++ registrasiPengguna/views.py
@@ -1,32 +1,4 @@
-# from django.db import connection
-# from collections import namedtuple
-# from django.shortcuts import render
-# from django.db import connection
-# from collections import namedtuple
-#
-# # Create your views here.
-# def index(request):
-#     return render(request, 'index.html')
-#
-# def namedtuplefetchall(cursor):
-#     "Return all rows from a cursor as a namedtuple"
-#     desc = cursor.description
-#     nt_result = namedtuple('Result', [col[0] for col in desc])
-#     return [nt_result(*row) for row in cursor.fetchall()]
-#
-# def index(request):
-#     cursor = connection.cursor()
-#     try:
-#         cursor.execute("SET SEARCH_PATH TO THECIMS")
-#         cursor.execute("SELECT USERNAME FROM AKUN")
-#         cursor.execute("SELECT USERNAME, EMAIL FROM PEMAIN")
-#         result = namedtuplefetchall(cursor)
-#     except Exception as e:
-#         print(e)
-#     finally:
-#         cursor.close()
-#
-#     return render(request, 'index.html', {'result': result})
+
 from registrasiPengguna.forms import *
 from django.db import connection
 from django.shortcuts import redirect, render
