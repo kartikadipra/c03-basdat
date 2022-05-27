5d378d1ea415114c649685459de6ecb341e524f0
M. Faiq Hilmi Musyaffa <m.faiq@ui.ac.id>
2022-05-27 18:24:54 +0700

    CRD kategori_apparel, koleksi, and koleksi_tokoh

--- koleksi_tokoh/views.py
+++ koleksi_tokoh/views.py
@@ -3,8 +3,9 @@ from django.db import connection
 from collections import namedtuple
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .forms import createForm
 
-def index(request):
+def rud_koleksi_tokoh(request):
     cursor = connection.cursor()
     try:
         cursor.execute("SET SEARCH_PATH TO THECIMS")
@@ -16,6 +17,32 @@ def index(request):
         cursor.close()
 
     return render(request, 'koleksi_tokoh/index.html', {'result': result})
+
+def c_koleksi_tokoh(request):
+    # if request.session['role'] != 'admin':
+    #     return HttpResponseRedirect(reverse('/'))
+    if request.method == 'POST':
+        form = createForm(request.POST)
+        if form.is_valid():
+            id_koleksi = form.cleaned_data['id_koleksi']
+            username_pengguna = form.cleaned_data['username_pengguna']
+            nama_tokoh = form.cleaned_data['nama_tokoh']
+            with connection.cursor() as c:
+                c.execute("SET SEARCH_PATH TO THECIMS")
+                c.execute(f"INSERT INTO KOLEKSI_TOKOH VALUES ('{id_koleksi}', '{username_pengguna}', '{nama_tokoh}')")
+        return HttpResponseRedirect(reverse('koleksi_tokoh:koleksi_tokoh'))
+
+    create_form = createForm()
+    response = {'create_form':create_form}
+    return render(request,'koleksi_tokoh/create.html', response)
+
+def d_koleksi_tokoh(request, id_koleksi, username_pengguna, nama_tokoh):
+    # if request.session['role'] != 'admin':
+    #     return HttpResponseRedirect(reverse('/'))
+    with connection.cursor() as c:
+        c.execute("SET SEARCH_PATH TO THECIMS")
+        c.execute("DELETE FROM KOLEKSI_TOKOH WHERE id_koleksi = %s AND username_pengguna = %s AND nama_tokoh = %s", [id_koleksi, username_pengguna, nama_tokoh])
+    return HttpResponseRedirect(reverse('koleksi_tokoh:koleksi_tokoh'))
 
 def namedtuplefetchall(cursor):
     "Return all rows from a cursor as a namedtuple"
